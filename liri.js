28b5537ff477b870ff8e8fb8cd2e845b4a8ddb64
thescottbob <scottmcmains@gmail.com>
2018-10-18 13:26:56 -0700

    Updated liri.js

--- liri.js
+++ liri.js
@@ -1,11 +1,34 @@
-require("dotenv").config();
-
-var spotify = new Spotify(keys.spotify);
-
-
-// Make it so liri.js can take in one of the following commands:
+// Make it so liri.js can take in all of the following commandss:
 
 // concert-this
 // spotify-this-song
 // movie-this
 // do-what-it-says
+
+require("dotenv").config();
+
+function Spotify() {
+  // search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
+  
+  spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  
+  console.log(data); 
+  });
+
+};
+
+var spotify = new Spotify(keys.spotify);
+
+
+// node liri.js spotify-this-song '<song name here>'
+
+const result = dotenv.config()
+ 
+if (result.error) {
+  throw result.error
+}
+ 
+console.log(result.parsed)
